7bfc38698e0b6a0b84561bc0f8a35bfc9bf34976
Ant00000ny <Ant00000ny@icloud.com>
2024-12-08 16:11:46 +0800

    feat: 添加声音和随机效果

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 'use client'
 import Image from "next/image";
 import {motion} from "motion/react";
+import React, {RefObject, useRef} from "react";
+
 
 export default function Home() {
-  const playAudio = () => {
-    // todo
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const handleClick = () => {
+    if (audioRef.current) {
+      audioRef.current!.src = '/audio/' + files[Math.floor(Math.random() * files.length)]
+      audioRef.current!.load()
+      audioRef.current!.play()
+    }
   }
 
   return (
@@ -15,14 +22,36 @@ export default function Home() {
                   dragElastic={0.05}
                   whileHover={{scale: 1.05}}
                   whileTap={{scale: 0.95}}
-                  onClick={playAudio}>
+                  onClick={handleClick}>
         <Image draggable={false}
                src={`/对吗.png`}
                width={478}
                height={230}
                alt={``}>
         </Image>
+        <Audio src={`/audio/对吗1.wav`} ref={audioRef}/>
       </motion.div>
     </div>
   );
 }
+
+const Audio: React.FC<{ src: string, ref: RefObject<HTMLAudioElement | null> }>
+  = ({src, ref}) => {
+  return (
+    <audio className={`hidden`} ref={ref} preload='auto' src={src}></audio>
+  )
+}
+
+const files = [
+  '对吗1.wav',
+  '不对1.wav',
+  '不对2.wav',
+  '不对3.wav',
+  '不对4.wav',
+  '不对5.wav',
+  '不对6.wav',
+  '对的1.wav',
+  '对的2.wav',
+  '对的3.wav',
+  '对的4.wav',
+]
