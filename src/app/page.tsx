5efcf0b859e2fa030b08462513b422428716baae
Ant00000ny <Ant00000ny@icloud.com>
2024-12-08 21:03:00 +0800

    refactor: 提取组件

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,19 @@ const files = [
 ]
 
 export default function Home() {
+
+
+  return (
+    <div className={`w-screen h-screen flex justify-center items-center px-5`}>
+      <HearthStoneEmote/>
+    </div>
+  );
+}
+
+const HearthStoneEmote = () => {
   const audioRef = useRef<HTMLAudioElement>(null);
-  const handleClick = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const playAudio = () => {
     if (audioRef.current) {
       const randomFile = files[Math.floor(Math.random() * files.length)];
       audioRef.current!.src = `/audio/${randomFile}`
@@ -30,28 +41,22 @@ export default function Home() {
     }
   }
 
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  return (
-    <div className={`w-screen h-screen flex justify-center items-center px-5`}>
-      <motion.div drag
-                  dragConstraints={{top: 0, right: 0, bottom: 0, left: 0}}
-                  dragElastic={0.05}
-                  whileHover={{scale: 1.05}}
-                  whileTap={{scale: 0.95}}
-                  onClick={handleClick}
-                  className={isPlaying ? `opacity-50 pointer-events-none` : ``}>
-        <Image draggable={false}
-               src={`/对吗.png`}
-               width={478}
-               height={230}
-               alt={``}>
-        </Image>
-        <Audio src={`/audio/对吗1.wav`} ref={audioRef}/>
-      </motion.div>
-    </div>
-  );
-}
+  return <motion.div drag
+                     dragConstraints={{top: 0, right: 0, bottom: 0, left: 0}}
+                     dragElastic={0.05}
+                     whileHover={{scale: 1.05}}
+                     whileTap={{scale: 0.95}}
+                     onClick={playAudio}
+                     className={isPlaying ? `opacity-50 pointer-events-none` : ``}>
+    <Image draggable={false}
+           src={`/对吗.png`}
+           width={478}
+           height={230}
+           alt={``}>
+    </Image>
+    <Audio src={`/audio/对吗1.wav`} ref={audioRef}/>
+  </motion.div>;
+};
 
 const Audio: React.FC<{ src: string, ref: RefObject<HTMLAudioElement | null> }>
   = ({src, ref}) => {
