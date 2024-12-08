f5f7359f9347e98bb8fc0ef623511e6c6d5810cd
Ant00000ny <Ant00000ny@icloud.com>
2024-12-08 16:28:43 +0800

    feat: 置灰效果

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,46 @@
 'use client'
 import Image from "next/image";
 import {motion} from "motion/react";
-import React, {RefObject, useRef} from "react";
+import React, {RefObject, useRef, useState} from "react";
 
+const files = [
+  '对吗1.wav',
+  '不对1.wav',
+  '不对2.wav',
+  '不对3.wav',
+  '不对4.wav',
+  '不对5.wav',
+  '不对6.wav',
+  '对的1.wav',
+  '对的2.wav',
+  '对的3.wav',
+  '对的4.wav',
+]
 
 export default function Home() {
   const audioRef = useRef<HTMLAudioElement>(null);
   const handleClick = () => {
     if (audioRef.current) {
-      audioRef.current!.src = '/audio/' + files[Math.floor(Math.random() * files.length)]
+      const randomFile = files[Math.floor(Math.random() * files.length)];
+      audioRef.current!.src = `/audio/${randomFile}`
       audioRef.current!.load()
       audioRef.current!.play()
+      setIsPlaying(true);
+      audioRef.current.onended = () => setIsPlaying(false);
     }
   }
+
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className={`w-screen h-screen flex justify-center items-center`}>
       <motion.div drag
-                  whileDrag={{}}
-                  dragConstraints={{left: 1, right: 1, top: 1, bottom: 1}}
+                  dragConstraints={{top: 0, right: 0, bottom: 0, left: 0}}
                   dragElastic={0.05}
                   whileHover={{scale: 1.05}}
                   whileTap={{scale: 0.95}}
-                  onClick={handleClick}>
+                  onClick={handleClick}
+                  className={isPlaying ? `opacity-50 pointer-events-none` : ``}>
         <Image draggable={false}
                src={`/对吗.png`}
                width={478}
@@ -42,16 +60,3 @@ const Audio: React.FC<{ src: string, ref: RefObject<HTMLAudioElement | null> }>
   )
 }
 
-const files = [
-  '对吗1.wav',
-  '不对1.wav',
-  '不对2.wav',
-  '不对3.wav',
-  '不对4.wav',
-  '不对5.wav',
-  '不对6.wav',
-  '对的1.wav',
-  '对的2.wav',
-  '对的3.wav',
-  '对的4.wav',
-]
